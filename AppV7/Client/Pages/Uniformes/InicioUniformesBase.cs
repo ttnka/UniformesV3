db5df74ae77891b89d2b2add9f5cb97bf48cfb28
ttnka <7545183+ttnka@users.noreply.github.com>
2022-11-28 11:27:41 -0700

    28 Nov Medio dia iniciamos

--- AppV7/Client/Pages/Uniformes/InicioUniformesBase.cs
+++ AppV7/Client/Pages/Uniformes/InicioUniformesBase.cs
@@ -11,11 +11,19 @@ namespace AppV7.Client.Pages.Uniformes
         public int SelectedIndex { get; set; } = 0;
         public int showRenglon { get; set; } = 1;
         public double showavance { get; set; } = 0;
+        public string TxtMetaGral { get; set; } = string.Empty;
         public double DataAlmacenes { get; set; } = 0;
+        public string DataAlmacenesName { get; set; } = string.Empty;
         public double DataComercios { get; set; } = 0;
+        public string TxtDataComercios { get; set; } = string.Empty;
         public double DataFoliosHillo { get; set; } = 0;
+        public string TxtDataFoliosHillo { get; set; } = string.Empty;
         public double DataFoliosGuaymas { get; set; } = 0;
-        public string DataAlmComer { get; set; } = string.Empty;
+        public double DataFoliosNogales { get; set; } = 0;
+        public string TxtDataFoliosGuaymas { get; set; } = string.Empty;
+        public string TxtDataFoliosNogales { get; set; } = string.Empty;
+        public string TxtDataAlmComer { get; set; } = string.Empty;
+        public string TxtMpios { get; set; } = string.Empty;
         public Dictionary<string, string> Graficas { get; set; } = 
             new Dictionary<string, string>();
 
@@ -23,9 +31,9 @@ namespace AppV7.Client.Pages.Uniformes
         {
             await LeerGraficos();
             showRenglon = MyFunc.DameRandom(1, 100);
-            showavance = MyFunc.DameRandom(1, 100);
+            //showavance = MyFunc.DameRandom(1, 100);
+            Console.WriteLine(showavance);
         }
-
         protected async Task LeerGraficos()
         {
             // General  Folios Entregados /  Meta GENERAL 
@@ -34,25 +42,31 @@ namespace AppV7.Client.Pages.Uniformes
             LasMetas = await MetasIServ.Buscar("Alla");
             var General = LasMetas.FirstOrDefault(x => x.Titulo == "GENERAL").Cantidad;
             if (General < 1) General = 1;
-            showavance = (100) * (LosFolios.Count() / General);
-
-
+            TxtMetaGral = General.ToString();
+            showavance = ((100) * (LosFolios.Count()) / General) < 1 ? 1 : 
+                ((100) * (LosFolios.Count()) / General);
+            
             // Almacenes y Comercioss sumatoria de los 2
             LosAlmacenes = await AlmacenIServ.Buscar("Alla");
             DataAlmacenes = LosAlmacenes.Count() == 0 ? 1 : LosAlmacenes.Count();
             
-            LosComerciantes = await UsersIServ.Buscar("Alla", "Vacio");
+            LosComerciantes = await UsersIServ.Buscar("All", "Vacio");
             DataComercios = LosComerciantes.Count(x => x.Nivel == 2) == 0 ?
                                 1 : LosComerciantes.Count(x => x.Nivel == 2);
 
-            DataAlmComer = $"{DataAlmacenes},{DataComercios}";
+            TxtDataAlmComer = $"{DataAlmacenes},{DataComercios}";
+            
             // Folios entregados en Guyamas y Hermosillo
 
-            DataFoliosGuaymas = LosFolios.Count(x => x.Municipio == "Guaymas") == 0 ?
-                1 : LosFolios.Count(x => x.Municipio == "Guaymas");
-            DataFoliosHillo = LosFolios.Count(x => x.Municipio == "Hermosillo") == 0 ?
-                1 : LosFolios.Count(x => x.Municipio == "Hermosillo");
+            DataFoliosNogales = LosFolios.Count(x => x.Localidad == "Nogales") == 0 ?
+                1 : LosFolios.Count(x => x.Localidad == "Nogales");
+            TxtDataFoliosNogales = DataFoliosNogales.ToString();
 
+            DataFoliosHillo = LosFolios.Count(x => x.Localidad == "Hermosillo") == 0 ?
+                1 : LosFolios.Count(x => x.Localidad == "Hermosillo");
+            TxtDataFoliosHillo = DataFoliosHillo.ToString();
+
+            TxtMpios = $"{TxtDataFoliosHillo},{TxtDataFoliosNogales}";
             
         } 
 
